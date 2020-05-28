deae16d8eff8f70f23c503ecb345575750f822c3
Dani Blackwell <bleuxgenius@gmail.com>
2020-05-28 14:55:15 -0500

    Ability to click buttons and create shapes

---
+++ gameoflife/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Gameboard from './Gameboard.js'
+
+function App() {
+  return (
+    <div className="App">
+     <Gameboard />
+    </div>
+  );
+}
+
+export default App;
